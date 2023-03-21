b66efd893588b8c9c4d096089ac90ef150ff6496
caiogallobarreira <caiogallobarreira@gmail.com>
2023-03-21 11:17:23 -0300

    * alteração url Mapping do EstoqueController

--- src/main/java/br/com/fiap/cde/controllers/EstoqueController.java
+++ src/main/java/br/com/fiap/cde/controllers/EstoqueController.java
@@ -9,12 +9,12 @@ import br.com.fiap.cde.models.Estoque;
 @RestController
 public class EstoqueController {
  
-    @GetMapping("/api/estoque")
+    @GetMapping("/api/v1/estoque")
     public Estoque index(){
         return new Estoque(1l, "Teste", "Teste");
     }
 
-    @GetMapping("/api/estoque/{id}")
+    @GetMapping("/api/v1/estoque/{id}")
     public Estoque show(@PathVariable Long id){
         return new Estoque(id, "Teste", "Teste");
     }

36622118f77cd7abf0594196e269d286e391bec3
caiogallobarreira <caiogallobarreira@gmail.com>
2023-03-21 11:36:39 -0300

    * delete e update

--- src/main/java/br/com/fiap/cde/controllers/ProdutoController.java
+++ src/main/java/br/com/fiap/cde/controllers/ProdutoController.java
@@ -8,9 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,11 +35,11 @@ public class ProdutoController {
     @GetMapping("/api/v1/produto/{id}")
     public ResponseEntity<Produto> show(@PathVariable Long id){
         logger.info("Listando produto: " + id);
-        Optional<Produto> produto = produtos.stream().filter(p -> p.getId().equals(id)).findFirst();
-        if (produto.isEmpty()) 
+        Optional<Produto> produtoOptional = produtos.stream().filter(p -> p.getId().equals(id)).findFirst();
+        if (produtoOptional.isEmpty()) 
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
-        return ResponseEntity.ok(produto.get());
+        return ResponseEntity.ok(produtoOptional.get());
     }
 
     @PostMapping("/api/v1/produto")
@@ -45,6 +47,34 @@ public class ProdutoController {
         logger.info("Produto criado com sucesso! " + produto);
         produtos.add(produto);
         return ResponseEntity.status(HttpStatus.CREATED).body(produto);
+    }
+
+    @DeleteMapping("/api/v1/produto/{id}")
+    public ResponseEntity<Produto> delete(@PathVariable Long id){
+        logger.info("Produto deletado com sucesso! " + id);
+        Optional<Produto> produtoOptional = produtos.stream().filter(p -> p.getId().equals(id)).findFirst();
+        if (produtoOptional.isEmpty()) 
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        produtos.remove(produtoOptional.get());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/api/v1/produto/{id}")
+    public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto produto){
+        logger.info("Produto atualizado com sucesso! " + produto);
+        Optional<Produto> produtoOptional = produtos.stream().filter(p -> p.getId().equals(id)).findFirst();
+        if (produtoOptional.isEmpty()) 
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        Produto produtoAtualizado = produtoOptional.get();
+        produtoAtualizado.setNome(produto.getNome());
+        produtoAtualizado.setDescricao(produto.getDescricao());
+        produtoAtualizado.setImagemUrl(produto.getImagemUrl());
+        produtoAtualizado.setQuantidade(produto.getQuantidade());
+        produtoAtualizado.setQuantidadeMinima(produto.getQuantidadeMinima());
+        
+        return ResponseEntity.ok(produtoAtualizado);
     }
 
 }

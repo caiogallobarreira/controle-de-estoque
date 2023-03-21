93f47fd35098b5d36940f13950259f1f96ed172f
caiogallobarreira <caiogallobarreira@gmail.com>
2023-03-21 08:52:42 -0300

    * estoquecontroller

---
+++ src/main/java/br/com/fiap/cde/controllers/EstoqueController.java
@@ -0,0 +1,21 @@
+package br.com.fiap.cde.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.cde.models.Estoque;
+
+@RestController
+public class EstoqueController {
+ 
+    @GetMapping("/api/estoque")
+    public Estoque index(){
+        return new Estoque(1l, "Teste", "Teste");
+    }
+
+    @GetMapping("/api/estoque/{id}")
+    public Estoque show(@PathVariable Long id){
+        return new Estoque(id, "Teste", "Teste");
+    }
+}

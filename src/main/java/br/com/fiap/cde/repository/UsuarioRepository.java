26e97771e760008173eb72d6a62b4e3a94c1e397
caiogallobarreira <caiogallobarreira@gmail.com>
2023-04-18 13:38:01 -0300

    * findByName do usuario

--- src/main/java/br/com/fiap/cde/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/cde/repository/UsuarioRepository.java
@@ -1,6 +1,10 @@
 package br.com.fiap.cde.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.fiap.cde.models.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{}
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+    Page<Usuario> findByNomeContaining(String search, Pageable pageable);}

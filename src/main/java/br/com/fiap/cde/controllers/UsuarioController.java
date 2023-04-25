3d8a9ef3b57a853f7740c1aebd9f4ae78d2667ad
caiogallobarreira <caiogallobarreira@gmail.com>
2023-04-25 10:36:32 -0300

    * removido import inutil

--- src/main/java/br/com/fiap/cde/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/cde/controllers/UsuarioController.java
@@ -1,6 +1,5 @@
 package br.com.fiap.cde.controllers;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
